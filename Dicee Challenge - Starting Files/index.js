7756cbe844723d4dd8c3c442dd997216c2048263
Cesar Rodriguez <streamlink777@live.com>
2020-02-19 13:11:12 -0500

    first commit

---
+++ Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,24 @@
+function randomNumber1(){
+    var number1 = Math.floor((Math.random() * 6)+1);
+    document.querySelector('img').setAttribute('src',"images/dice"+number1+".png" );
+    return number1
+}
+
+function randomNumber2(){
+    var number2 = Math.floor((Math.random() * 6)+1);
+    document.querySelectorAll('img')[1].setAttribute('src', "images/dice"+number2+".png");
+    return number2
+}
+
+
+function activate(){
+    var number1 = randomNumber1();
+    var number2 = randomNumber2();
+    if(number1 > number2){
+        document.querySelector('h1').innerText = 'Player 1 Wins';
+    }else if(number1 < number2) {
+        document.querySelector('h1').innerText = 'Player 2 Wins';
+    } else {
+        document.querySelector('h1').innerText = 'Draw';
+    }
+}
