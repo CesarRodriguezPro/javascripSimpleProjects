2003a4e4e57d6b148b9dbf49475149d3de8f258b
Cesar Rodriguez <streamlink777@live.com>
2020-04-27 19:02:46 -0400

    finish v1 todo list

--- todoList_v1/app.js
+++ todoList_v1/app.js
@@ -3,14 +3,18 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 app.set('view engine', 'ejs');
+
+var inputValue;
+var items = [];
+
 
 app.listen(3000, function(){
     console.log('Server Is Running');
 });
 
-var inputValue;
-var items = [];
+
 app.get('/', function(req, res){
 
     var today = new Date();
@@ -22,6 +26,7 @@ app.get('/', function(req, res){
     var day = today.toLocaleDateString("en-US", options);
     res.render('lists',{kindOfDay:day, newListItem:items});
 });
+
 
 app.post('/', function(req, res){
     inputValue = req.body.newItem;
