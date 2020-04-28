6d450518c681964107c22400151fa0572f2ce034
Cesar Rodriguez <streamlink777@live.com>
2020-04-27 22:39:57 -0400

    adding work list to program

--- todoList_v1/app.js
+++ todoList_v1/app.js
@@ -8,6 +8,7 @@ app.set('view engine', 'ejs');
 
 var inputValue;
 var items = [];
+var workItems = [];
 
 
 app.listen(3000, function(){
@@ -16,7 +17,6 @@ app.listen(3000, function(){
 
 
 app.get('/', function(req, res){
-
     var today = new Date();
     var options = {
         weekday:'long',
@@ -24,13 +24,21 @@ app.get('/', function(req, res){
         month:"long",
     }
     var day = today.toLocaleDateString("en-US", options);
-    res.render('lists',{kindOfDay:day, newListItem:items});
+    res.render('lists',{listTitle:day, newListItem:items});
 });
 
 
 app.post('/', function(req, res){
     inputValue = req.body.newItem;
-    items.push(inputValue);
-    res.redirect('/');
-});
+    if(res.body.list === "Work"){
+        workItems.push(item);
+        res.redirect('/work');
+    }else{
+        items.push(inputValue);
+        res.redirect('/');
+    }});
 
+
+app.get('/work', function(req,res){
+    res.render("lists", {listTitle:"Work List", newListItem:workItems})
+});
