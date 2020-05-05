071af234179cc4ee86fb69816085b23fd4ce7b50
Cesar Rodriguez <merlin@Cesars-MacBook-Pro.local>
2020-05-04 21:25:00 -0400

    working in simple Blog

--- todoList_v1/app.js
+++ todoList_v1/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const date = require(__dirname+"/date.js")
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
@@ -11,34 +14,42 @@ var items = [];
 var workItems = [];
 
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log('Server Is Running');
 });
 
 
-app.get('/', function(req, res){
-    var today = new Date();
-    var options = {
-        weekday:'long',
-        day: "numeric",
-        month:"long",
-    }
-    var day = today.toLocaleDateString("en-US", options);
-    res.render('lists',{listTitle:day, newListItem:items});
+app.get('/', function (req, res) {
+    let day = date();
+    res.render("lists", {
+        listTitle: day,
+        newListItem: items
+    });
 });
 
 
-app.post('/', function(req, res){
+app.get('/work', function (req, res) {
+    res.render("lists", {
+        listTitle: "Work List",
+        newListItem: workItems
+    })
+});
+
+
+app.post('/', function (req, res) {
     inputValue = req.body.newItem;
-    if(res.body.list === "Work"){
-        workItems.push(item);
-        res.redirect('/work');
-    }else{
+    let valueOfList = req.body.list
+
+    if (valueOfList === "Work") {
+        workItems.push(inputValue);
+        res.redirect("/work");
+    } else {
         items.push(inputValue);
-        res.redirect('/');
-    }});
+        res.redirect("/")
+    }
+});
 
 
-app.get('/work', function(req,res){
-    res.render("lists", {listTitle:"Work List", newListItem:workItems})
+app.get('/about', function(req, res){
+    res.render("about")
 });
