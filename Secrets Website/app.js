ccb4f43cbfd913c42d6d98920224048b1dbe9389
Cesar Rodriguez <streamlink777@live.com>
2020-05-25 16:50:39 -0400

    working in level 4 security in secrets website

--- Secrets Website/app.js
+++ Secrets Website/app.js
@@ -4,80 +4,105 @@ const express = require("express");
 const bodyParser = require('body-parser');
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
-const app = express();
+const bcrypt = require("bcrypt");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
+
+const app = express();
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(session({
+    secret: 'there in not place like home',
+    resave: false,
+    saveUninitialized: false,
+  }))
+app.use(passport.initialize());
+app.use(passport.session());
 
 
+/////////////////////////////////   database set up     ///////////////////////////////// 
 mongoose.connect("mongodb://localhost:27017/userDB", {
     useNewUrlParser: true,
     useUnifiedTopology: true
-})
-
-// for integrating encrytion level 2 we need to add a proper mongoose Schema
+});
 const userSchema = new mongoose.Schema({
-    email:String,
-    password:String
+    email: String,
+    password: String
 });
 
-// i learn to use env with dotenv
-userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields:['password']});
-const User = mongoose.model("User", userSchema)
+userSchema.plugin(passportLocalMongoose);
+const User = mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+///////////////////////////////  bcrypt set up  //////////////////////////////////////////
+const saltRounds = 10;
 
 
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
     res.render('home');
 });
 
-
-app.get("/login", function(req, res){
+app.get("/login", function (req, res) {
     res.render('login');
 });
 
-
-app.get("/register", function(req, res){
+app.get("/register", function (req, res) {
     res.render('register');
 });
 
+app.post('/register', function (req, res) {
 
-app.post('/register', function(req, res){
 
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password,
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+
+        const newUser = new User({
+            email: req.body.username,
+            password: hash,
+        });
+        newUser.save(function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render('secrets');
+            }
+        })
+
     });
-    newUser.save(function(err){
+});
+
+app.post('/login', function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({
+        email: username
+    }, function (err, foundUser) {
         if (err) {
             console.log(err);
-        }else{
-            res.render('secrets');
+        } else {
+            if (foundUser) {
+                bcrypt.compare(password, foundUser.password, function (err, result) {
+                    if (result === true) {
+                        res.render('secrets');
+                    }else{
+                        console.log(err);
+                    }
+                });
+            }
         }
     });
 });
 
-
-app.post('/login', function(req, res){
-    const username = req.body.username;
-    const password = req.body.password;
-    
-    User.findOne({email:username}, function(err, foundUser){
-       if(err){
-           console.log(err);
-       }else{
-           if(foundUser){
-               if(foundUser.password === password){
-                   res.render('secrets');
-               }
-           }
-       }
-    })
-})
-
-
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Server Running");
 });
